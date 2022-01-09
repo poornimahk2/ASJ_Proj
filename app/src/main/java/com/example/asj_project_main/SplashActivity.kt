3ca7ff1b0d223562eef0b5542531bf888c3fbab1
poornimahari123@gmail.com <02052001ph>
2022-01-09 21:00:58 +0530

    Commit

--- app/src/main/java/com/example/asj_project_main/SplashActivity.kt
+++ app/src/main/java/com/example/asj_project_main/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.example.asj_project_main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.asj_project_main.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -12,8 +13,11 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-        finish()
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
 
     }
 }
